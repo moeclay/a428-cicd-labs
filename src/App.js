855e0e7894cd221b052496ef5dce0b0fd9ca59eb
moeclay <moeamarreza@gmail.com>
2023-07-25 15:32:58 +0700

    proyek akhir 19

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!</h1>
+          <h1 className="App-title">Proyek Akhir CI/CD</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Latihan CI/CD dengan Jenkins, Prometheus &amp; Grafana.
         </p>
       </div>
     );
